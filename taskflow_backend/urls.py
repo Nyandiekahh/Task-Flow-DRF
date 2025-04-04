3776f037d29754e66ceceb051ccb5a3fec48442c
Nyandiekahh <einsteinmokua100@gmail.com>
2025-04-04 09:56:34 +0300

    Organizational Update

--- taskflow_backend/urls.py
+++ taskflow_backend/urls.py
@@ -1,5 +1,3 @@
-# taskflow_backend/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -8,6 +6,7 @@ urlpatterns = [
     path('', RedirectView.as_view(url='admin/', permanent=False)),  # Redirect root to admin
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-    path('api/calendar/', include('calendar_events.api.urls')),  # Add this new line
-    path('roles/', include('roles.urls')),  # Add this new line
+    path('api/v1/auth/', include('accounts.urls')),  # Add this line
+    path('api/calendar/', include('calendar_events.api.urls')),
+    path('roles/', include('roles.urls')),
 ]
